8926105653715a76362b85a3b5d65a0e060132d6
glidyon <drippler16@gmail.com>
2021-04-24 02:48:11 +0200

    problem no. 1313

---
+++ leetcode/1313. Decompress Run-Length Encoded List/problem_1313.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        vector<int> result;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i % 2 == 0) {
+                for (int j = 0; j < nums[i]; j++) {
+                    result.push_back(nums[i + 1]);
+                }
+            }
+        }
+        return result;
+    }
+};
