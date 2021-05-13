25991766e562ffc34d1d4b7990e6ab23978618cc
unknown <skociektoja@gmail.com>
2021-05-13 07:10:15 +0200

    added wrong subtraction problem solution

---
+++ codeforces/977A_Wrong Subtraction/wrong_subtraction.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+
+	int n, k;
+	std::cin >> n >> k;
+
+	for(int i = 0; i < k; i++){
+		if(n % 10 != 0){
+			n--;
+		}
+		else{
+			n /= 10;
+		}
+	}
+
+
+	std::cout << n << std::endl;
+
+	return 0;
+}
