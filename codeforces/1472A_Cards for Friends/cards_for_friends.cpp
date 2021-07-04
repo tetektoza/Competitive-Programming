02b46383856bb7558c229ed08fe3f488c2cfb239
glidyon <skociektoja@gmail.com>
2021-07-04 23:11:15 +0200

    code simplifying for 1472A

--- codeforces/1472A_Cards for Friends/cards_for_friends.cpp
+++ codeforces/1472A_Cards for Friends/cards_for_friends.cpp
@@ -9,29 +9,20 @@ int main(){
     for(int i = 0; i < t; i++){
         std::cin >> w >> h >> n;
         
-        int flag = 0, dist = 1;
-        
-        if(n == 1){
-            flag = 1;
-            goto end;
-        }
-        
-        if(w % 2 == 0 || h % 2 == 0){
-            while(dist < n && (w % 2 == 0 || h % 2 == 0)){
-                if(w % 2 == 0){
-                    w /= 2;
-                    dist *= 2;
-                }   
-                else if(h % 2 == 0){
-                    h /= 2;
-                    dist *= 2;
-                }
+        int dist = 1;
+        while(dist < n && (w % 2 == 0 || h % 2 == 0)){
+            if(w % 2 == 0){
+                w /= 2;
+                dist *= 2;
             }   
-        }
+            else if(h % 2 == 0){
+                h /= 2;
+                dist *= 2;
+            }
+        }   
 
-        end:
         
-        if(dist >= n || flag == 1){
+        if(dist >= n){
             std::cout << "YES" << std::endl;
         }
         else{
