0522e91c6219465de0215722900b53bd3d2403c6
glidyon <skociektoja@gmail.com>
2021-07-10 04:42:49 +0200

    added again twenty five! problem solution

---
+++ codeforces/630A_Again Twenty Five!/again_twenty_five.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+// Math problems are the weirdest ones on this site, I swear..
+int main(){
+
+    int n;
+    std::cin >> n;
+
+
+    std::cout << 25 << std::endl;
+
+
+
+    return 0;
+}
