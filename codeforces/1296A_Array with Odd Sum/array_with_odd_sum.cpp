15a4ea4df051754d972154127e65d13b55f197ee
glidyon <skociektoja@gmail.com>
2021-07-10 22:49:05 +0200

    code simplification for 1296A

--- codeforces/1296A_Array with Odd Sum/array_with_odd_sum.cpp
+++ codeforces/1296A_Array with Odd Sum/array_with_odd_sum.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 
-
 int main(){
 
     int t, n;
@@ -10,13 +9,13 @@ int main(){
     for(int i = 0; i < t; i++){
         std::cin >> n;
 
-        auto arr = new int[n];
+        int num;
 
         int sum = 0, odd = 0;
         for(int i = 0; i < n; i++){
-            std::cin >> arr[i];
-            sum += arr[i];
-            if(arr[i] % 2 != 0){
+            std::cin >> num;
+            sum += num;
+            if(num % 2 != 0){
                 odd++;
             }
         }
@@ -29,8 +28,6 @@ int main(){
             std::cout << "NO" << std::endl;
         }
 
-
-        delete[] arr;
     }
 
 
