7d7d9d5a19b73baae71259ae18bc5b208957f60c
glidyon <skociektoja@gmail.com>
2021-07-12 05:40:52 +0200

    added mahmoud and ehab and the even-odd game

---
+++ codeforces/959A_Mahmoud and Ehab and the even-odd game/mahmoud_and_ehab_and_the_even_odd_game.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+
+    int n;
+    std::cin >> n;
+    
+    if(n % 2 == 0){
+        std::cout << "Mahmoud" << std::endl;
+    }
+    else{
+        std::cout << "Ehab" << std::endl;
+    }
+
+
+
+    return 0;
+}
