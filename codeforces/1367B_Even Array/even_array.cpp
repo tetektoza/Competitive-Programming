93914d93970b486d1c6b9289aa9a2ec27d908713
glidyon <skociektoja@gmail.com>
2021-07-12 16:47:20 +0200

    added even array problem solution

---
+++ codeforces/1367B_Even Array/even_array.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+
+int main(){
+
+    int t, n;
+
+    std::cin >> t;
+
+    for(int i = 0; i < t; i++){
+        std::cin >> n;
+
+        auto arr = new int[n];
+        
+
+        for(int i = 0; i < n; i++){
+            std::cin >> arr[i];
+        }
+
+        int flag = 0, tmp = 0, moves = 0;
+        for(int i = 0; i < n; i++){
+            if((arr[i] % 2 != 0 && i % 2 == 0) || (arr[i] % 2 == 0 && i % 2 != 0)){
+                flag = 1;
+                for(int j = 0; j < n; j++){
+                    if(i == j){
+                        continue;
+                    }
+
+                    if((arr[i] % 2 != 0 && i % 2 == 0) && (arr[j] % 2 == 0 && j % 2 != 0)){
+                        std::swap(arr[i], arr[j]);
+                        flag = 0;
+                        moves++;    
+                    }
+                    else if((arr[i] % 2 == 0 && i % 2 != 0) && (arr[j] % 2 != 0 && j % 2 == 0)){
+                        std::swap(arr[i], arr[j]);
+                        flag = 0;
+                        moves++;
+                    }
+                }
+
+                if(flag){
+                    break;
+                }
+            }
+        }
+        
+        if(flag){
+            std::cout << -1 << std::endl;
+        }
+        else{
+            std::cout << moves << std::endl;
+        }
+
+
+        delete[] arr;
+
+    }
+
+
+    return 0;
+}
