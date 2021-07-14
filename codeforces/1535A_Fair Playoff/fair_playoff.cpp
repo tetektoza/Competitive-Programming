10044b18b30e0e97ec1e6baeaf86320472bc6919
glidyon <skociektoja@gmail.com>
2021-07-14 05:21:29 +0200

    added fair playoff problem solution

---
+++ codeforces/1535A_Fair Playoff/fair_playoff.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+
+
+int main(){
+    
+    int t, n;
+
+    std::cin >> t;
+
+    for(int i = 0; i < t; i++){
+
+        auto arr = new int[4];
+
+        for(int i = 0; i < 4; i++){
+            std::cin >> arr[i];
+        }
+        
+        int first_ind, second_ind;
+
+        if(arr[0] > arr[1]){
+            first_ind = 0;
+        }
+        else{
+            first_ind = 1;
+        }
+
+        if(arr[2] > arr[3]){
+            second_ind = 2;
+        }
+        else{
+            second_ind = 3;
+        }
+        
+        int flag = 0;
+        for(int i = 0; i < 4; i++){
+            if(i == first_ind || i == second_ind){
+                continue;
+            }
+
+            if(arr[first_ind] < arr[i] || arr[second_ind] < arr[i]){
+                flag = 1;
+                break;
+            }
+        }
+
+        if(flag){
+            std::cout << "NO" << std::endl;
+        }
+        else{
+            std::cout << "YES" << std::endl;
+        }
+    
+        delete[] arr;
+    }
+
+
+
+    return 0;
+}
