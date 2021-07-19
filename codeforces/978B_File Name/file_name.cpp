c388e8581975e5152214b37b69ec3b2a7b7b9e44
glidyon <skociektoja@gmail.com>
2021-07-19 06:20:26 +0200

    added file name problem solution

---
+++ codeforces/978B_File Name/file_name.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+
+
+int main(){
+    int n;
+    std::string file_name;
+    std::cin >> n >> file_name;
+    
+    int cnt = 0, total = 0;
+    for(int i = 0; i < n; i++){
+        if(file_name[i] == 'x'){
+            cnt++;
+        }
+        else{
+            if(cnt > 2){
+                total += (cnt-2);
+            }
+
+            cnt = 0;
+        }
+
+    }
+
+
+    if(cnt < 3){
+        std::cout << total << std::endl;
+    }
+    else{
+        std::cout << (cnt-2)+total << std::endl;
+    }
+
+    return 0;
+}
