f79ce626bd2ea9500fc0b68f1ad6125fbe2776af
glidyon <skociektoja@gmail.com>
2021-07-22 05:03:21 +0200

    added 599A code simplification

--- codeforces/599A_Patrick and Shopping/patrick_and_shopping.cpp
+++ codeforces/599A_Patrick and Shopping/patrick_and_shopping.cpp
@@ -12,19 +12,7 @@ int main(){
 
     long long d1_d2 = 2*d1+2*d2, d1_d3 = 2*d1+2*d3, d2_d3 = 2*d2+2*d3, d1_d2_d3 = d1+d2+d3;
     
-    long long ans;
-    if(d1_d2 <= d1_d3 && d1_d2 <= d2_d3 && d1_d2 <= d1_d2_d3){
-        ans = d1_d2;
-    }
-    else if(d1_d3 <= d1_d2 && d1_d3 <= d2_d3 && d1_d3 <= d1_d2_d3){
-        ans = d1_d3;
-    }
-    else if(d2_d3 <= d1_d2 && d2_d3 <= d1_d3 && d2_d3 <= d1_d2_d3){
-        ans = d2_d3;
-    }
-    else{
-        ans = d1_d2_d3;
-    }
+    long long ans = std::min({d1_d2, d1_d3, d2_d3, d1_d2_d3});    
 
     std::cout << ans << std::endl;
 
