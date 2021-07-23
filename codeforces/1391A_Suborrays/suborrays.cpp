4ea83293126861bc105406ef2a42faa143576cd9
glidyon <skociektoja@gmail.com>
2021-07-23 05:03:37 +0200

    added suboarrays problem solution

---
+++ codeforces/1391A_Suborrays/suborrays.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+
+
+
+int main(){
+
+    int t;
+    std::cin >> t;
+    while(t--){
+        int n;
+        std::cin >> n;
+
+        for(int i = n; i > 0; i--){
+            std::cout << i << " ";
+        }
+
+        std::cout << std::endl;
+
+    }
+
+
+    return 0;
+}
