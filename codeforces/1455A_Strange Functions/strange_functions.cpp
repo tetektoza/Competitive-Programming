ce6e99bb5b3adc3c27bab3726a635c3be1a9cb9d
glidyon <skociektoja@gmail.com>
2021-07-23 07:33:22 +0200

    added strange functions problem solution

---
+++ codeforces/1455A_Strange Functions/strange_functions.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+
+
+int main(){
+    int t;
+    std::cin >> t;
+    while(t--){
+        std::string n;
+        std::cin >> n;
+        
+        std::cout << n.size() << std::endl;
+    }
+
+
+
+
+    return 0;
+}
