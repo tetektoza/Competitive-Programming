9e389b0c834d98b9cb2e5051a4751c7ac687f236
glidyon <skociektoja@gmail.com>
2021-08-09 02:52:35 +0200

    added ehab and another construction problem

---
+++ codeforces/1088A_Ehab and another construction problem/ehab_and_another_construction_problem.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+
+int main(){
+    
+    int x; cin >> x;
+
+    if(x == 1){
+        cout << -1 << endl;
+        return 0;
+    }
+
+    cout << x << " " << x << endl;
+
+    return 0;
+}
