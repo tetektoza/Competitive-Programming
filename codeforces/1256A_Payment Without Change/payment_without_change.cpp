d0e18e2619455435fdf4715f8b75779ce09762ed
glidyon <skociektoja@gmail.com>
2021-08-27 00:16:57 +0200

    added change to problem 1256, CF

--- codeforces/1256A_Payment Without Change/payment_without_change.cpp
+++ codeforces/1256A_Payment Without Change/payment_without_change.cpp
@@ -3,18 +3,16 @@
 
 using namespace std;
 
-
+typedef long long ll;
 int main(){
     int q;
     cin >> q;
     while(q--){
-        int a, b, n, S;
+        ll a, b, n, S;
         cin >> a >> b >> n >> S;
         
-        int numa = min(S/n, a);
-        ((a*n >= S && S%n == 0)
-        || b >= S
-        || (a >= numa && S-(numa*n) <= b))
+        ll numa = min(S/n, a);
+        ((a*n >= S && S%n == 0) || b >= S || (a >= numa && S-(numa*n) <= b))
         ? cout << "YES" << '\n' : cout << "NO" << '\n';   
 
     }
